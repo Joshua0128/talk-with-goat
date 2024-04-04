c5d4e61204d3492f27f4d819da32fedb3e11f396
Joshua0128 <waitingwu.cai09g@nctu.edu.tw>
2024-04-05 00:26:41 +0800

    chore: initial the css and setup the prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	semi: false,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 100,
+	tabWidth: 2,
+	importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	plugins: [
+		'prettier-plugin-tailwindcss',
+		'@trivago/prettier-plugin-sort-imports',
+	],
+}

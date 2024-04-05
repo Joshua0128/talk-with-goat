45633453cf671be1d07152176bad7257a1aeaa8d
Joshua0128 <waitingwu.cai09g@nctu.edu.tw>
2024-04-05 15:26:54 +0800

    feat: add auth with Clerk

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+const RootPage = () => {
+	return (
+		<div>
+			<UserButton afterSignOutUrl="/"/>
+			Root Page(Protected)
+		</div>
+	);
+}
+ 
+export default RootPage;
